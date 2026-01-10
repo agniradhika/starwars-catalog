5a917ddf720cf6c5265dcc22d36d2821e32aa804
Radhika <agniradhika@users.noreply.github.com>
2026-01-10 14:34:37 +0100

    add simple card grid layout with tailwind

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
         "https://swapi.info/api/people"
       );
       const data = await response.json();
-     // console.log(data);
+      //console.log(data);
       setData(data);
     };
 
@@ -21,17 +21,25 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Star Wars Characters</h1>
+    <div class="max-w-5xl mx-auto bg-white rounded-xl shadow-md p-6" className="App">
+      <h1 class="text-lg font-semibold text-gray-800 mb-6">Star Wars Characters</h1>
       {data ? (
-        <ul>
-          {data.map((character) => (
-            <li key={character.name}>{character.name}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <ul class="grid grid-cols-4 gap-6">
+        {data.map((character) => (
+        <li key={character.name} class="rounded-xl border border-gray-200 p-4 text-center hover:bg-gray-50 transition">
+          <a href="#A" class="block transition">
+          <span class="block text-xl font-mono font-semibold text-gray-900 mb-2">
+          {character.name}
+          </span>
+          <span class="text-sm text-gray-500">
+          Eye color: {character.eye_color}
+          </span>
+          </a>
+        </li>))}
+            </ul>
+          ) : (
+            <p>Loading...</p>
+          )}
     </div>
   );
 }
