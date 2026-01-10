99844b8b3d1f67032d99ccfd2bd84f4e069c9bfa
Radhika <agniradhika@users.noreply.github.com>
2026-01-10 14:34:19 +0100

    simple fetchData with async await

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
 import './App.css';
+import { useEffect, useState } from "react";
+
 
 function App() {
+    const [data, setData] = useState(null);
+
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://swapi.info/api/people"
+      );
+      const data = await response.json();
+     // console.log(data);
+      setData(data);
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="App">
+      <h1>Star Wars Characters</h1>
+      {data ? (
+        <ul>
+          {data.map((character) => (
+            <li key={character.name}>{character.name}</li>
+          ))}
+        </ul>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
   );
 }
 
